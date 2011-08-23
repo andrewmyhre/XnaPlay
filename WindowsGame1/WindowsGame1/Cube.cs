ca11049d8871a40d903caebf23483203cf255ce0
Andrew Myhre <andrew.myhre@gmail.com>
2011-08-23 23:07:19 +0100

    initial import

---
+++ WindowsGame1/WindowsGame1/Cube.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tad.Xna.Common.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1
+{
+    public class Cube : GameEntity
+    {
+        private BasicEffect effect;
+        public float _sideLength;
+        CubeFace[] faces;
+        private int[] Indices;
+        private VertexPositionColor[] Vertices;
+
+        public Cube(Game game, Vector3 position, float sideLength) : base(game)
+        {
+            this._position = position;
+            this._sideLength = sideLength;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            VertexPositionColor[] vertices = new VertexPositionColor[]
+            {
+                new VertexPositionColor(new Vector3(-_sideLength,-_sideLength,-_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(_sideLength,-_sideLength,-_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(_sideLength,-_sideLength,_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(-_sideLength,-_sideLength,_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(-_sideLength,_sideLength,-_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(_sideLength,_sideLength,-_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(_sideLength,_sideLength,_sideLength), Color.White),
+                new VertexPositionColor(new Vector3(-_sideLength,_sideLength,_sideLength), Color.White),
+            };
+
+            faces = new CubeFace[] {
+                new CubeFace(this, vertices[0], vertices[1], vertices[2], vertices[3])/*,
+                new CubeFace(this, vertices[4], vertices[0], vertices[3], vertices[7]),
+                new CubeFace(this, vertices[5], vertices[6], vertices[7], vertices[5]),
+                new CubeFace(this, vertices[1], vertices[2], vertices[6], vertices[5]),
+                new CubeFace(this, vertices[6], vertices[2], vertices[3], vertices[7])*/
+            };
+
+            effect = new BasicEffect(GraphicsDevice);
+
+            List<VertexPositionColor> verts = new List<VertexPositionColor>();
+            List<int> indices = new List<int>();
+            foreach(var face in faces)
+            {
+                foreach (var triangle in face.Triangles)
+                {
+                    if (!verts.Contains(triangle._v1))
+                        verts.Add(triangle._v1);
+                    if (!verts.Contains(triangle._v2))
+                        verts.Add(triangle._v2);
+                    if (!verts.Contains(triangle._v3))
+                        verts.Add(triangle._v3);
+
+                    indices.Add(verts.IndexOf(triangle._v1));
+                    indices.Add(verts.IndexOf(triangle._v2));
+                    indices.Add(verts.IndexOf(triangle._v3));
+                }
+            }
+
+            Vertices = vertices.ToArray();
+            Indices = indices.ToArray();
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach(var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                
+                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList,
+                                     Vertices, 0, Vertices.Length, Indices, 0, Indices.Length/3);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+
+    public class CubeFace
+    {
+        Cube _cube;
+        public CubeTriangle[] Triangles;
+
+        public CubeFace(Cube cube, 
+            VertexPositionColor v1, 
+            VertexPositionColor v2, 
+            VertexPositionColor v3, 
+            VertexPositionColor v4)
+        {
+            _cube = cube;
+            Triangles = new CubeTriangle[]
+            {
+                new CubeTriangle(cube, this, null, v1, v2, v3),
+                new CubeTriangle(cube, this, null, v2, v3, v4)
+            };
+        }
+
+        public void LoadContent()
+        {
+        }
+    }
+
+    public class CubeTriangle
+    {
+        private  Cube _cube;
+        private  CubeFace _face;
+        private CubeTriangle[] children;
+        private  CubeTriangle _parent;
+        public readonly VertexPositionColor _v1;
+        public readonly VertexPositionColor _v2;
+        public readonly VertexPositionColor _v3;
+
+        public CubeTriangle(Cube cube, CubeFace face, CubeTriangle parent,
+            VertexPositionColor v1,
+            VertexPositionColor v2,
+            VertexPositionColor v3)
+        {
+            _cube = cube;
+            _face = face;
+            _parent = parent;
+            _v1 = v1;
+            _v2 = v2;
+            _v3 = v3;
+        }
+    }
+}
---
+++ WindowsGame1/WindowsGame1/Globe.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Tad.Xna.Common.Cameras;
+using Tad.Xna.Common.Entities;
+
+namespace WindowsGame1
+{
+    public class Globe : GameEntity
+    {
+        BasicEffect effect;
+        private VertexBuffer testVBuffer;
+        private IndexBuffer testIBuffer;
+        private VertexPositionColor[] vertices;
+        private int[] indices;
+
+        public Globe(Game game) : base(game)
+        {
+            IsStatic = true;
+        }
+
+        public override void Initialize()
+        {
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            float X = 30, Z = 30;
+            vertices = new VertexPositionColor[]
+            {
+                new VertexPositionColor(new Vector3(-X, 0.0f, Z), Color.White),
+                new VertexPositionColor(new Vector3(X, 0.0f, Z), Color.White),
+                new VertexPositionColor(new Vector3(-X, 0.0f, -Z), Color.White),
+                new VertexPositionColor(new Vector3(X, 0.0f, -Z), Color.White),   
+                new VertexPositionColor(new Vector3(0.0f, Z, X), Color.White),
+                new VertexPositionColor(new Vector3(0.0f, Z, -X), Color.White),
+                new VertexPositionColor(new Vector3(0.0f, -Z, X), Color.White),
+                new VertexPositionColor(new Vector3(0.0f, -Z, -X), Color.White),  
+                new VertexPositionColor(new Vector3(Z, X, 0.0f), Color.White),
+                new VertexPositionColor(new Vector3(-Z, X, 0.0f), Color.White),
+                new VertexPositionColor(new Vector3(Z, -X, 0.0f), Color.White),
+                new VertexPositionColor(new Vector3(-Z, -X, 0.0f), Color.White),
+            };
+            indices = new int[]
+            {
+                0,4,1, 
+                0,9,4, 
+                9,5,4, 
+                4,5,8, 
+                4,8,1,    
+                8,10,1, 
+                8,3,10, 
+                5,3,8, 
+                5,2,3, 
+                2,7,3,    
+                7,10,3, 
+                7,6,10, 
+                7,11,6, 
+                11,0,6, 
+                0,1,6, 
+                6,1,10, 
+                9,0,11, 
+                9,11,2,
+                9,2,5, 
+                7,2,11            
+            };
+
+            testVBuffer = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+            testVBuffer.SetData(vertices);
+
+            testIBuffer = new IndexBuffer(Game.GraphicsDevice, typeof(int), indices.Length, BufferUsage.WriteOnly);
+            testIBuffer.SetData(indices);
+
+            effect = new BasicEffect(Game.GraphicsDevice);
+            effect.EnableDefaultLighting();
+            effect.LightingEnabled = false;
+            effect.AmbientLightColor = new Vector3(255, 255, 255);
+
+        }
+
+        private static RasterizerState _wireframe = new RasterizerState() {FillMode = FillMode.WireFrame};
+        private static RasterizerState _solid = new RasterizerState() { FillMode = FillMode.Solid};
+
+        public override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.SetVertexBuffer(testVBuffer);
+            GraphicsDevice.Indices = testIBuffer;
+
+            effect.Projection = Camera.Default.ProjectionMatrix;
+            effect.View = Camera.Default.ViewMatrix;
+            effect.World = Matrix.Identity
+                 * Matrix.CreateTranslation(this.Position);
+
+            if (DrawWireframe)
+                GraphicsDevice.RasterizerState = _wireframe;
+            else
+                GraphicsDevice.RasterizerState = _solid;
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList,
+                    vertices, 0, vertices.Length, indices, 0, indices.Length/3);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
